73005452339b1ded2f18daa6cdd20f5e4def74f5
Mrasifplm1 <maxregner1@web.de>
2021-01-23 19:43:10 -0500

    first commit

--- config.sh
+++ config.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-GITHUB_USER=Akianonymus
-GITHUB_EMAIL=anonymus.aki@gmail.com
+GITHUB_USER=Mrasifplm1
+GITHUB_EMAIL=maxregner1@web.de
 
 KBUILD_BUILD_USER="Aki"
 KBUILD_BUILD_HOST="A_DEAD_PLANET"
 
 export oem=motorola
-export device=harpia
+export device=troika
 
 ROM="PixelExperience"
 manifest_url="https://github.com/PixelExperience/manifest"
 export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
 branch="pie"
 
-release_repo="Akianonymus/harpia_builds"
+release_repo="Mrasifplm1/troika"
 
 timezone="Asia/Kolkata"
